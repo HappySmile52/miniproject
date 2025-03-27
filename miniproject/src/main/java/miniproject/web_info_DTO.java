2a63a081d4912cdf8bbe95ddb0ae0c9bca88020a
HappySmile52 <alice050777@naver.com>
2025-03-27 16:27:53 +0900

    5, 6 파일 업로드

---
+++ miniproject/src/main/java/miniproject/web_info_DTO.java
@@ -0,0 +1,12 @@
+package miniproject;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class web_info_DTO {
+	String web_company, web_ceo, web_add, web_num;
+	String web_sales, web_inc, web_tel, web_fax;
+}
--- miniproject/src/main/java/miniproject/web_info_mapper.java
+++ miniproject/src/main/java/miniproject/web_info_mapper.java
@@ -1,5 +1,8 @@
 package miniproject;
 
-public class web_info_mapper {
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
+public interface web_info_mapper {
+	web_info_DTO web_info(String midx);
 }
